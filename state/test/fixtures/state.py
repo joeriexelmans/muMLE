633aa84f39a255050832056822eaf942355bf3f8
Andrei Bondarenko <andrei.i.bondarenko@gmail.com>
2021-08-11 11:46:41 +0200

    Added back RDF and Neo4j implementations

--- state/test/fixtures/state.py
+++ state/test/fixtures/state.py
@@ -1,13 +1,13 @@
 import pytest
 from state.pystate import PyState
-# from state.rdfstate import RDFState
-# from state.neo4jstate import Neo4jState
+from state.rdfstate import RDFState
+from state.neo4jstate import Neo4jState
 
 
 @pytest.fixture(params=[
     (PyState,),
-    # (RDFState, "http://example.org/#"),
-    # (Neo4jState,)
+    (RDFState, "http://example.org/#"),
+    (Neo4jState,)
 ])
 def state(request):
     if len(request.param) > 1:
@@ -15,5 +15,5 @@ def state(request):
     else:
         state = request.param[0]()
     yield state
-    # if isinstance(state, Neo4jState):
-    #     state.close(clear=True)
+    if isinstance(state, Neo4jState):
+        state.close(clear=True)
