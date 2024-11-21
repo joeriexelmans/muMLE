c9c5a5214d7b9bf1a0d0ae7c29e05c575c9a1c40
Joeri Exelmans <joeri.exelmans@gmail.com>
2024-11-21 14:17:50 +0100

    add petrinet 'mutex'-example from MoSIS slides

--- examples/petrinet/runner.py
+++ examples/petrinet/runner.py
@@ -23,8 +23,10 @@ if __name__ == "__main__":
     # Read models from their files
     mm_cs           =         read_file('metamodels/mm_design.od')
     mm_rt_cs        = mm_cs + read_file('metamodels/mm_runtime.od')
-    m_cs            =         read_file('models/m_example_simple.od')
-    m_rt_initial_cs = m_cs +  read_file('models/m_example_simple_rt_initial.od')
+    # m_cs            =         read_file('models/m_example_simple.od')
+    # m_rt_initial_cs = m_cs +  read_file('models/m_example_simple_rt_initial.od')
+    m_cs            =         read_file('models/m_example_mutex.od')
+    m_rt_initial_cs = m_cs +  read_file('models/m_example_mutex_rt_initial.od')
 
     # Parse them
     mm           = loader.parse_and_check(state, mm_cs,           scd_mmm, "Petri-Net Design meta-model")
